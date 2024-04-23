6bcb5e89965d2d2130f5d3bf846aa885d7d64daf
masfazan <msfazan@icloud.com>
2024-04-23 09:41:58 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿int pos, penultimo = -1, atual, ultimo = 1;
+    
+Console.WriteLine("Informe a posição limite: ");
+pos= int.Parse(Console.ReadLine());
+
+
+    for (int i = 0; i < pos; i++)
+    {
+        atual = penultimo + ultimo;
+        Console.WriteLine(atual);
+        penultimo = ultimo;
+        ultimo = atual;
+    }
